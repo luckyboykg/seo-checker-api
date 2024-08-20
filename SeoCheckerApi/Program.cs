100047d6d9e7d20fc12feb72dde2a3d8d0e2a746
Tăng Thanh tâm <trieutulong89kg@gmail.com>
2024-08-20 13:45:53 +0700

    chore: refactor code

--- SeoCheckerApi/Program.cs
+++ SeoCheckerApi/Program.cs
@@ -11,51 +11,66 @@ using SeoCheckerApi.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddHttpClient();
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddMemoryCache();
-
-builder.Services.AddCors(o => o.AddPolicy("SeoCheckerCors", option =>
-{
-    option.WithOrigins(builder.Configuration.GetValue<string>("SeoChecker:SpaUrl") ?? "SpaUrl")
-           .AllowAnyMethod()
-           .AllowAnyHeader()
-           .AllowCredentials();
-}));
-
-// Services
-builder.Services.AddScoped<IMemoryCacheService, MemoryCacheService>();
-builder.Services.AddScoped<IGoogleSearchService, GoogleSearchService>();
-builder.Services.AddScoped<IBingSearchService, BingSearchService>();
-
-// MediatR
-builder.Services.AddMediatR(p => p.RegisterServicesFromAssembly(typeof(GetSeoInfoFromGoogleRequest).Assembly));
-builder.Services.AddMvc(options => { options.Filters.Add(new BadRequestActionFilter()); });
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<GetSeoInfoFromGoogleRequestValidator>();
+ConfigureServices(builder);
+ConfigureCors(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-app.UseCors("SeoCheckerCors");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigureMiddleware(app);
+ConfigureEndpoints(app);
 
 app.Run();
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddMemoryCache();
+
+    builder.Services.AddCors(o => o.AddPolicy("SeoCheckerCors", option =>
+    {
+        option.WithOrigins(builder.Configuration.GetValue<string>("SeoChecker:SpaUrl") ?? "SpaUrl")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    }));
+
+    // Services
+    builder.Services.AddScoped<IMemoryCacheService, MemoryCacheService>();
+    builder.Services.AddScoped<IGoogleSearchService, GoogleSearchService>();
+    builder.Services.AddScoped<IBingSearchService, BingSearchService>();
+}
+
+void ConfigureCors(WebApplicationBuilder builder)
+{
+    builder.Services.AddMediatR(p => p.RegisterServicesFromAssembly(typeof(GetSeoInfoFromGoogleRequest).Assembly));
+    builder.Services.AddMvc(options => { options.Filters.Add(new BadRequestActionFilter()); });
+    builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssemblyContaining<GetSeoInfoFromGoogleRequestValidator>();
+}
+
+void ConfigureMiddleware(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    app.UseCors("SeoCheckerCors");
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+}
+
+void ConfigureEndpoints(WebApplication app)
+{
+    app.MapControllers();
+}
